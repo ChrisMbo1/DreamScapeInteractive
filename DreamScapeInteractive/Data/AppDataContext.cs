8e3f210048de74692e8c8fd10522ae459221d642
Chris.p <149588945+ChrisMbo1@users.noreply.github.com>
2026-02-27 15:09:09 +0100

    Add project files.

---
+++ DreamScapeInteractive/Data/AppDataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DreamScapeInteractive.Model;
+
+namespace DreamScapeInteractive.Data
+{
+    public class AppDataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;port=3306;user=root;password=;database=DreamScapeInteractive;",
+                ServerVersion.AutoDetect("server=localhost;port=3306;user=root;password=;database=DreamScapeInteractive;")
+            );
+
+        }
+    }
+}
---
+++ DreamScapeInteractive/Model/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamScapeInteractive.Model
+{
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+
+        //fk
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+        public User()
+        {
+        }
+
+    }
+}
